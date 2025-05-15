6e465326fb0f64c41de4cf1ecb408f6d83311859
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-15 15:23:13 +0300

    Updated settings.py and .gitignore

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "debug_toolbar",
+    "user",
+    "theatre"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "user.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",  # або AllowAny на старті
+    ],
+}
